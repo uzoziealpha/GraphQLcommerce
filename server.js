2a5adb2fd9b2ba29f05b87459d4eada817e1f4fc
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-13 10:27:02 +0800

    making our products return the data asynchronously

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const schema = makeExecutableSchema({
     typeDefs: typesArray,
     resolvers: {
-        
+        Query: {
+        products: async(parent) => {
+            console.log('Getting the products....');
+            const product = await  Promise.resolve(parent.products);
+            return product;
+        },
+        orders: (parent) => {
+            console.log('Getting orders....');
+            return parent.orders;
+        },
     }
+  }
 });
 
 
